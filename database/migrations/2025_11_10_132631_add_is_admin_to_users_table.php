e6913701bf7b2b010ea8adbb3f74467d4c22ab41
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-11-10 18:28:23 +0500

    Add is_admin column

---
+++ database/migrations/2025_11_10_132631_add_is_admin_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Hum email column ke baad ek naya 'is_admin' column add kar rahe hain
+            $table->boolean('is_admin')->default(false)->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
